12fe1db962d75af1bee764e0c6427dead9b555f0
190507 <190507@e0505-39.JC-21.local>
2020-12-08 14:06:26 +0900

    メインプログラムの作成 テストの作成

---
+++ src/Kadai06Test.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Kadai07Test extends Kadai06 {
+
+	@Test
+	void get2ndMax() {
+
+		Kadai06 k6= new Kadai06();
+		int a[] = {4,2,1,7,6};
+		assertEquals(6 , k6.get2ndMax(a));
+
+		int b[] = {3,56,21,8,63,13,88,42,99,35};
+		assertEquals(88 , k6.get2ndMax(b));
+
+
+
+
+
+	}
+
+}

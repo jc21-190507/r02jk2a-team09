60e7fcaee986deef5ff8fde610db82d40ffc0a9e
190118 <190118@e0505-36.JC-21.local>
2020-12-08 16:29:09 +0900

    Kadai02変更しました。

--- src/Kadai02.java
+++ src/Kadai02.java
@@ -5,10 +5,19 @@ public class Kadai02 {
 	 * 課題02
 	 * 文字列の中に、指定されたアルファベットが何文字入っているかを返すメソッド
 	 * @param s 文字列
-	 * @param c 文字
+	 * @param c
 	 * @return sの中に入っていたcの文字数
 	 */
-	int countString(String s,char c) {
-		return -1;
+	int countString(String s, char c) {
+		char[] str = s.toCharArray();
+		int i,counter = 0;
+		for(i  =  0;i < str.length ; i ++ ) {
+			if(c == str[i] )
+				counter++;
+		}
+		return counter;
 	}
-}
+
+
+	}
+

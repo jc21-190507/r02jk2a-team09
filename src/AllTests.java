3f09a811959901f6b5a5c747a9085baf3c82c4e8
yoshzawa <yoshzawa@gmail.com>
2021-01-29 22:48:54 +0900

    テスト６，７実行確認済み、テスト１未完成

---
+++ src/AllTests.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+//	Kadai01Test.class, // テスト未完成
+	Kadai06Test.class,
+	Kadai07Test.class
+})
+public class AllTests {
+
+}
